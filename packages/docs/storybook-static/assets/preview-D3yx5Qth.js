989f9107dcef3cba499554ac8819d58f3d9d0c2f
Gabriel Lima <37782627+glpar@users.noreply.github.com>
2026-08-14 14:44:41 -0300

    Melhora a documentação em áreas compactas
    Motivo: reduzir o espaçamento do Storybook e permitir rolagem segura nas tabelas de tokens dentro do popup.

--- packages/docs/storybook-static/assets/preview-D3yx5Qth.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-BCZNEkbS.js","assets/chunk-HLWAVYOI-Cuox5Pa4.js","assets/iframe-aNcK3Apg.js","assets/index-CBqU2yxZ.js","assets/_commonjsHelpers-BosuxZz1.js","assets/react-18-D8cruF67.js","assets/index-BtM5VmRH.js","assets/chunk-EIRT5I3Z-CbnHSNsj.js","assets/index-B_J8iUie.js","assets/extends-CCbyfPlC.js","assets/index-Cmc67Rxs.js","assets/index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-aNcK3Apg.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BCZNEkbS.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]));return new r}}};export{a as parameters};
---
+++ packages/docs/storybook-static/assets/preview-DGVnrYhw.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-1nXmRGd5.js","assets/chunk-HLWAVYOI-CFj5qctZ.js","assets/iframe-C4tJdFBX.js","assets/index-CBqU2yxZ.js","assets/_commonjsHelpers-BosuxZz1.js","assets/react-18-D8cruF67.js","assets/index-BtM5VmRH.js","assets/chunk-EIRT5I3Z-CbnHSNsj.js","assets/index-B_J8iUie.js","assets/extends-CCbyfPlC.js","assets/index-Cmc67Rxs.js","assets/index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as e}from"./iframe-C4tJdFBX.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-1nXmRGd5.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]));return new r}}};export{a as parameters};
